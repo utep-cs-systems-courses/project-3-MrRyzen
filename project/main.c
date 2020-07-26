f0f2118e0ef73f69e851c4f10a4073b501ea74b0
Cristobal Medina <cristobalmd01@gmail.com>
2020-07-25 19:22:34 -0600

    Drawing triangle

--- project/main.c
+++ project/main.c
@@ -17,8 +17,12 @@ void main(void) {
 
   drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
   drawString8x12 (10,10,"Howdy", COLOR_WHITE, COLOR_SKY_BLUE);
-
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  
+  for (int col = 0; col < 20; col++) {
+    for (int row = 0; row < col; row++) {
+      void drawPixel(col, row, COLOR_ORANGE);
+    }
+  }
 
   /* lab 2
   switch_init();
