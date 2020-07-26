3cc78f4e4e4d41f3c8a4f44641cbf468f8f1c8bc
Cristobal Medina <cristobalmd01@gmail.com>
2020-07-25 18:38:12 -0600

    changed makefile

--- project/main.c
+++ project/main.c
@@ -2,9 +2,14 @@
 #include <libTimer.h>
 #include <lcdutils.h>
 #include <lcddraw.h>
+#include <p2switches.h>
+#include "led.h"
+#include "switches.h"
+#include "buzzer.h"
 
 void main(void) {  
   configureClocks();
+  //lcd init
   lcd_init();
   u_char width = screenWidth, height = screenHeight;
 
@@ -15,13 +20,18 @@ void main(void) {
   drawString8x12 (10,10,"Howdy", COLOR_WHITE, COLOR_SKY_BLUE);
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+
+  // from p2sw-demo
+  p2sw_init(15);
+  or_sr(0x8); // GIE (enable interrupts)
   
-  /*switch_init();
+  //lab 2
+  switch_init();
   led_init();
   buzzer_init();
   enableWDTInterrupts();
 
   state_init();
   
-  or_sr(0x18);  */
+  or_sr(0x18);
 } 
