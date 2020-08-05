f4dde033add1f60d43da0b166e889d33ecd7c9b6
Cristobal Medina <cristobalmd01@gmail.com>
2020-08-04 18:47:31 -0600

    fixed errors

--- project/main.c
+++ project/main.c
@@ -33,14 +33,14 @@ void main() {
   P1OUT &= ~LED_GREEN;//cpu off
 
   or_sr(0x8);
-  
+
   for(;;) { 
     while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
-      P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
+      P1OUT &= ~LED_GREEN;    /**< Green led off witHo CPU */
       or_sr(0x10);	      /**< CPU OFF */
     }
-    P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
+    P1OUT |= LED_GREEN;       /**< Green led on when CPU on */
     redrawScreen = 0;
-    movLayerDraw(&ml0, &layer0);
+    drawState();
   }
 }
